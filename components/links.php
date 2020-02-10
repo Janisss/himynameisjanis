5086556a7d111c3ba22f8d5dad74df2abd6901b8
Janis iMac <imac@imacs-iMac.local>
2020-02-10 22:36:38 +0100

    start somehow

---
+++ components/links.php
@@ -0,0 +1,7 @@
+<link rel="stylesheet" href="node_modules/normalize.css/normalize.css">
+<link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap-grid.min.css">
+<link rel="stylesheet" href="node_modules/animate.css/animate.min.css">
+<link rel="stylesheet" href="css/style.css">
+
+<script src="node_modules/jquery/dist/jquery.min.js"></script>
+<script src="node_modules/jquery-aniview/dist/jquery.aniview.js"></script>
---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="sk">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>himynameisjanis.com</title>
+
+    <!-- METADATA -->
+    <?php include "components/metadata.php" ?>
+
+    <!-- LINKS -->
+    <?php include "components/links.php" ?>
+
+</head>
+<body>
+
+<img data-av-animation="zoomIn" src="img/final_export_web.jpg" alt="himynameisjanis" class="aniview introimage">
+
+<main>
+
+</main>
+    
+
+    <script>
+        $('.aniview').AniView();
+    </script>
+</body>
+</html>
