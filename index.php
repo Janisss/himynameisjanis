59b57bc0d9d8a66bdaf8eec92c4f9bcd5e0212a1
Janis iMac <imac@imacs-iMac.local>
2020-02-23 20:03:42 +0100

    updates

--- index.php
+++ index.php
@@ -16,9 +16,9 @@
 
 <body>
 
-<div id="dimmer">
-    <span style="font-family: 'smooth';color: rgb(32, 32, 32);">loading...</span>
-</div>
+    <div id="dimmer">
+        <span style="font-family: 'smooth';color: rgb(32, 32, 32);">loading...</span>
+    </div>
 
     <div class="pt-3 pl-3 pr-3 pt-md-5 pl-md-5 pr-md-5">
         <div data-av-animation="zoomIn" class="aniview introimage">
@@ -36,7 +36,8 @@
             <h1>video</h1>
 
             <span class="subtitle">
-                Natáčanie reklamných spotov, hudobných klipov, produktových videí a podobne. Tvorba videí ma baví zo všetkého najviac. Pozrite si videá ktoré som natáčal a strihal, alebo minimálne
+                Natáčanie reklamných spotov, hudobných klipov, produktových videí a podobne. Tvorba videí ma baví zo
+                všetkého najviac. Pozrite si videá ktoré som natáčal a strihal, alebo minimálne
                 sa podielal na ich výrobe.
             </span>
         </div>
@@ -44,52 +45,34 @@
         <div class="container aniview" data-av-animation="fadeIn">
             <div class="video-slider">
                 <div>
-                    <div class="video-container">
-                        <iframe src="https://www.youtube.com/embed/aXS94zHrRhs" frameborder="0"
-                            allow="accelerometer; gyroscope;"
-                            allowfullscreen>
-                        </iframe>
-                    </div>
+                    <a href="https://youtu.be/aXS94zHrRhs" target="_blank">
+                        <img data-src="img/video/thumb_okhamoro.jpg" alt="Video" class="video-thumbs lazy">
+                    </a>
                 </div>
                 <div>
-                    <div class="video-container">
-                        <iframe src="https://www.youtube.com/embed/EJ9BBC72liw" frameborder="0"
-                            allow="accelerometer; gyroscope;"
-                            allowfullscreen>
-                        </iframe>
-                    </div>
+                    <a href="https://youtu.be/YgGA-Cb2hPk" target="_blank">
+                        <img data-src="img/video/thumb_vwzraz.jpg" alt="Video" class="video-thumbs lazy">
+                    </a>
                 </div>
                 <div>
-                    <div class="video-container">
-                        <iframe src="https://www.youtube.com/embed/YgGA-Cb2hPk" frameborder="0"
-                            allow="accelerometer; gyroscope;"
-                            allowfullscreen>
-                        </iframe>
-                    </div>
+                    <a href="https://youtu.be/DMRzkQWn1u8" target="_blank">
+                        <img data-src="img/video/thumb_splitboard.jpg" alt="Video" class="video-thumbs lazy">
+                    </a>
                 </div>
                 <div>
-                    <div class="video-container">
-                        <iframe src="https://www.youtube.com/embed/DMRzkQWn1u8" frameborder="0"
-                            allow="accelerometer; gyroscope;"
-                            allowfullscreen>
-                        </iframe>
-                    </div>
+                    <a href="https://youtu.be/1Gi2k3cPBrA" target="_blank">
+                        <img data-src="img/video/thumb_andrejcova.jpg" alt="Video" class="video-thumbs lazy">
+                    </a>
                 </div>
                 <div>
-                    <div class="video-container">
-                        <iframe src="https://www.youtube.com/embed/1Gi2k3cPBrA" frameborder="0"
-                            allow="accelerometer; gyroscope;"
-                            allowfullscreen>
-                        </iframe>
-                    </div>
+                    <a href="https://youtu.be/EJ9BBC72liw" target="_blank">
+                        <img data-src="img/video/thumb_freshisland.jpg" alt="Video" class="video-thumbs lazy">
+                    </a>
                 </div>
                 <div>
-                    <div class="video-container">
-                        <iframe src="https://www.youtube.com/embed/CmoD1rlGtsU" frameborder="0"
-                            allow="accelerometer; gyroscope;"
-                            allowfullscreen>
-                        </iframe>
-                    </div>
+                    <a href="https://youtu.be/CmoD1rlGtsU" target="_blank">
+                        <img data-src="img/video/thumb_rbfriend.jpg" alt="Video" class="video-thumbs lazy">
+                    </a>
                 </div>
             </div>
         </div>
@@ -97,7 +80,8 @@
         <div class="container p-5">
             <h1>foto</h1>
             <span class="subtitle">
-                Príroda, produkty, ľudia alebo služby. K videu patrí neodlúčíteľne aj fotka. Fotenie je pre mňa strašne zaujímavá a zábavná vec ktorá dokáže
+                Príroda, produkty, ľudia alebo služby. K videu patrí neodlúčíteľne aj fotka. Fotenie je pre mňa strašne
+                zaujímavá a zábavná vec ktorá dokáže
                 uschovať moment naveky.
             </span>
         </div>
@@ -119,7 +103,7 @@
                 <img alt="image" class="grid-item lazy" data-src="img/gallery/12.jpg">
                 <img alt="image" class="grid-item lazy" data-src="img/gallery/13.jpg">
                 <img alt="image" class="grid-item lazy" data-src="img/gallery/14.jpg">
-                <img alt="image" class="grid-item grid-item--width2 lazy" data-src="img/gallery/15.jpg"> 
+                <img alt="image" class="grid-item grid-item--width2 lazy" data-src="img/gallery/15.jpg">
                 <img alt="image" class="grid-item lazy" data-src="img/gallery/16.jpg">
                 <img alt="image" class="grid-item lazy" data-src="img/gallery/17.jpg">
             </div>
